61103d29f1eb9e89aae7be116f7915ce06be9d10
irine3355 <irine3355@gmail.com>
2024-10-21 16:51:17 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import loadable from '@loadable/component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const MainLayout = loadable(() => import('./layouts/MainLayout'));
+const NotFound = loadable(() => import('./commons/pages/NotFound'));
+const Main = loadable(() => import('./main/pages/Main')); // 메인페이지
+
+// 회원 페이지
+const Member = loadable(() => import('./routes/Member'));
+
+// 마이 페이지
+//const Mypage = loadable(() => import('./routes/Mypage'));
+
+// 식당 페이지
+//const Restaurant = loadable(() => import('./routes/Restaurant'));
+
+
+const routeUrlPaths = [
+  'member',
+
+];
+
+const App = () => {
+  const location = useLocation();
+  return routeUrlPaths.includes(location.pathname.split('/')[1]) ? (
+    <>
+      <Member />
+  
+    </>
+  ) : (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Main />} /> {/* 메인 페이지 */}
+        <Route path="*" element={<NotFound />} /> {/* 없는 페이지 */}
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
